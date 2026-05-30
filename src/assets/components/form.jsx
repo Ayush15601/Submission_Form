0a14afa0efe9dae36a3f085464abb3dfb5cd920b
Ayush15601 <aayushtewarit@gmail.com>
2026-05-30 16:45:02 +0530

    added file and url input fields

--- src/assets/components/form.jsx
+++ src/assets/components/form.jsx
@@ -9,6 +9,8 @@ function Form() {
   const [contact, setcontact] = useState("")
   const [gender, setgender] = useState("")
   const [sub, setsub] = useState({maths: true, hindi: false, english: false})
+  const [resume, setresume] = useState("")
+  const [url, seturl] = useState("")
 
 
   const changetick = (subj) => {
@@ -22,16 +24,16 @@ function Form() {
         <>
         <form action="#" method="get" className="f_form">
             
-            <label htmlFor="firstname"> First Name* </label> <br />
+            <label htmlFor="firstname"> First Name* </label> 
             <input type="text" id="firstname" placeholder="Enter your name" name="firstname" value={firstname} onChange={ (e) => setfirstname(e.target.value)} />
 
-            <label htmlFor="lastname"> Last Name </label> <br />
+            <label htmlFor="lastname"> Last Name </label> 
             <input type="text" id="lastname" placeholder="Enter your last name" name="lastname" value={lastname} onChange={(e) => setlastame(e.target.value)}/>
 
-            <label htmlFor="email"> Email </label> <br />
+            <label htmlFor="email"> Email </label> 
             <input type="email" id="email" placeholder="Enter your email" name="email" value={email} onChange={(e) => setemail(e.target.value)}/>
 
-            <label htmlFor="contact"> Contact </label> <br />
+            <label htmlFor="contact"> Contact </label> 
             <input type="tel" id="contact" placeholder="Enter your phone number" name="contact" value={contact} onChange={(e) => setcontact(e.target.value)}/>
 
             <label htmlFor="gender"> Gender </label> <br />
@@ -39,12 +41,20 @@ function Form() {
             <input type="radio" id="gender" name="gender" value="female" checked={gender == "female"} onChange={(e) => setgender(e.target.value)}/> Female
             <input type="radio" id="gender" name="gender" value="other" checked={gender == "other"} onChange={(e) => setgender(e.target.value)}/> Gender
 
-            <br /> <br /><label htmlFor="checkbox"> Your best subjects </label> <br /> <br />
+            <br /> <label htmlFor="checkbox"> Your best subjects </label> <br />
 
             {/* new observation unlocked on onChanged here. Why we used arrow fun inside onChanged?*/}
             <input type="checkbox" id="checkbox" name="maths" checked={sub.maths === true} onChange={() => changetick("maths")}/> Maths
             <input type="checkbox" id="checkbox" name="hindi" checked={sub.hindi === true} onChange={() => changetick("hindi")}/> Hindi
             <input type="checkbox" id="checkbox" name="english" checked={sub.english === true} onChange={() => changetick("english")}/> English
+
+            <br /> <label htmlFor="resume"> Upload Resume </label> 
+            <input type="file" id="resume" name="resume" value={resume} onChange={(e) => setresume(e.target.file[0])}/>
+
+            <br /> <label htmlFor="url"> Enter URL </label> 
+            <input type="url" id="url" name="url" value={url} onChange={(e) => seturl(e.target.value)}/>
+
+
 
         
         </form>
